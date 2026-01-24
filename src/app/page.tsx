925dc774bcdb32bce89f619f5b7285b66320c7ea
mitulvaniya14 <mitulvaniya@gmail.com>
2026-01-25 00:25:20 +0530

    Remove scroll indicator from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,16 +116,6 @@ export default function Home() {
           </motion.div>
         </div>
 
-        {/* Scroll Indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1, duration: 1 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/50"
-        >
-          <span className="text-xs uppercase tracking-widest">Scroll</span>
-          <div className="w-[1px] h-12 bg-gradient-to-b from-white/50 to-transparent" />
-        </motion.div>
       </section>
 
       {/* Featured Spots Section */}
